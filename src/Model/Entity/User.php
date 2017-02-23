4ac8262ae57bb92c27a10a1f48e0793a497903aa
Robin <w.robingross@gmail.com>
2017-02-23 13:25:27 +0100

    Initial Commit

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 23/02/2017
+ * Time: 13:07
+ */
+
+namespace App\Model\Entity;
+
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+class User extends Entity
+{
+
+    // Make all fields mass assignable except for primary key field "id".
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+
+    // ...
+
+    protected function _setPassword($password)
+    {
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+
+    // ...
+}
